fbc0e098171372913415dbfafbcb49eb3ca78788
KEVIN <chen.l.leng@gmail.com>
2022-05-23 23:47:43 +0800

    add customised application

---
+++ app/src/main/java/com/globalweather/android/Weather.kt
@@ -0,0 +1,21 @@
+package com.globalweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+
+// create global context for applicaiton
+class Weather : Application() {
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
