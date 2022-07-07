7c7f1c46dea16e2e7f3aa48cb11c3f778b8fc712
KEVIN <chen.l.leng@gmail.com>
2022-07-07 13:20:08 +0930

    PlaceAdapter, use viewBinding
    Signed-off-by: Leung <Chen.l.leung@gmail.com>

--- app/src/main/java/com/globalweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/globalweather/android/ui/place/PlaceAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.globalweather.android.R
+import com.globalweather.android.databinding.PlaceItemBinding
 import com.globalweather.android.logic.model.Place
 import com.globalweather.android.ui.weather.WeatherActivity
 
@@ -15,14 +16,22 @@ import com.globalweather.android.ui.weather.WeatherActivity
 class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val placeName: TextView = view.findViewById(R.id.placeName)
-        val placeAddress: TextView = view.findViewById(R.id.placeAddress)
+//    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val placeName: TextView = view.findViewById(R.id.placeName)
+//        val placeAddress: TextView = view.findViewById(R.id.placeAddress)
+//    }
+
+    inner class ViewHolder(var placeItemBinding: PlaceItemBinding) : RecyclerView.ViewHolder(placeItemBinding.root) {
+        fun bind(place: Place){
+            placeItemBinding.placeName.text = place.name
+            placeItemBinding.placeAddress.text = place.address
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
-        val holder = ViewHolder(view)
+        val binding = PlaceItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = ViewHolder(binding)
+
         holder.itemView.setOnClickListener{
             val position = holder.adapterPosition
             val place = placeList[position]
@@ -54,8 +63,7 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val place = placeList[position]
-        holder.placeName.text = place.name
-        holder.placeAddress.text = place.address
+        holder.bind(place)
     }
 
     override fun getItemCount() = placeList.size
